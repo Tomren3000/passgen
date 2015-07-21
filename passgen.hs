e4e38a5be4aad3576d0c97dbe9c51d800392300a
Trine Løcke <trine.granvej6@gmail.com>
2015-07-21 16:58:52 +0200

    Added support for command line parameters

--- passgen.hs
+++ passgen.hs
@@ -1,2 +1,20 @@
-main = putStrLn "Hej"
+import System.Environment
+
+main = getArgs >>= parseArgs
+
+help :: IO ()
+help = putStrLn "Denne tekst hjælper dig"
+
+parseArgs :: [String] -> IO ()
+parseArgs ("-l":a:[]) = putStrLn a
+parseArgs [] = help
+parseArgs ["-h"] = help
+parseArgs ["--help"] = help
+parseArgs ["-v"] = version
+parseArgs ["--version"] = version
+parseArgs [a] = help
+parseArgs (a:as) = help
+
+version :: IO ()
+version = putStrLn "passgen version 0.0.1\nCopyright (C) 2015 Trine Løcke"
 
