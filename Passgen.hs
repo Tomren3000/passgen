6c2d542383b38c336f13beeba8d4c19b57fc3c19
Morten Jensen <tomren3000@gmail.com>
2016-04-11 20:08:19 +0200

    altered many things but most important changed the way we manage arguments for passgen in a brilliant new way.

--- Passgen.hs
+++ Passgen.hs
@@ -1,48 +1,74 @@
-import System.Environment
-import System.Exit
-import System.Random
+-- import System.Environment
+-- import System.Exit
+-- import System.Random
+import System.Console.ParseArgs
+import Control.Monad
+
+data Option = OptionVersion | OptionHelp | OptionLength
+           deriving (Ord,Eq,Show)
+
+argd :: [Arg Option]
+argd = [
+  Arg { argIndex = OptionVersion,
+        argName  = Just "version",
+        argAbbr  = Just 'v',
+        argData  = Nothing,
+        argDesc  = "Show program version" },
+  Arg { argIndex = OptionHelp,
+        argName  = Just "help",
+        argAbbr  = Just 'h',
+        argData  = Nothing,
+        argDesc  = "Show program help" },
+  Arg { argIndex = OptionVersion,
+        argName  = Just "length",
+        argAbbr  = Just 'l',
+        argData  = argDataDefaulted "char-count" ArgtypeInt 12,
+        argDesc  = "Show program length" } ]
 
 main :: IO ()
-main = getArgs >>= parseArgs
+main = do
 
-parseArgs :: [String] -> IO ()
-parseArgs ("-l":n:[]) = putStrLn (generatePassword (read n :: Int)) >> exitSuccess
-parseArgs ["-v"] = putStrLn version >> exitSuccess
-parseArgs ["--version"] = putStrLn version >> exitSuccess
-parseArgs ["-h"] = putStrLn help >> exitSuccess
-parseArgs ["--help"] = putStrLn help >> exitSuccess
-parseArgs _ = putStrLn help >> exitFailure
 
-version :: String
-version = "passgen version 0.0.1"
+--main = getArgs >>= parseArgs
 
-help :: String
-help = "passgen usage:\n" ++
-       " -l n length of password\n" ++
-       " -v | --version program version\n" ++
-       " -h | --help this help\n" ++
-        "Example: passgen -l 10"
+--parseArgs :: [String] -> IO ()
+--parseArgs ("-l":n:[]) = putStrLn (generatePassword (read n :: Int)) >> exitSuccess
+--parseArgs ["-v"] = putStrLn version >> exitSuccess
+--parseArgs ["--version"] = putStrLn version >> exitSuccess
+--parseArgs ["-h"] = putStrLn help >> exitSuccess
+--parseArgs ["--help"] = putStrLn help >> exitSuccess
+--parseArgs _ = putStrLn help >> exitFailure
 
-generatePassword :: Int -> String
-generatePassword n = "Length of password is " ++ show n
+--version :: String
+--version = "passgen version 0.0.1"
+
+--help :: String
+--help = "passgen usage:\n" ++
+--       " -l n length of password\n" ++
+--       " -v | --version program version\n" ++
+--       " -h | --help this help\n" ++
+--        "Example: passgen -l 10"
+
+--generatePassword :: Int -> String
+--generatePassword n = "Length of password is " ++ show n
 
 --let Foo = (!!) validChars
 --fmap foo rollDice
 
-generatePassword' :: Int -> String -> String
-generatePassword' n s
-  | n < 1 = s
-  | otherwise = generatePassword' (n - 1) ((:) 'a' s)
+--generatePassword' :: Int -> String -> String
+-- generatePassword' n s
+  -- | n < 1 = s
+  -- | otherwise = generatePassword' (n - 1) ((:) 'a' s)
 
-rollDice :: IO Int
-rollDice = getStdRandom (randomR (0,length validChars - 1))
-
-validChars :: String
-validChars = "abcdefghjkmnpqrstuvxyz"
-
-
-
-validInput :: String -> Bool
-validInput s = case reads s :: [(Int,String)] of
-  [(n,"")] -> True
-  _ -> False
+-- rollDice :: IO Int
+-- rollDice = getStdRandom (randomR (0,length validChars - 1))
+--
+-- validChars :: String
+-- validChars = "abcdefghjkmnpqrstuvxyz"
+--
+--
+--
+-- validInput :: String -> Bool
+-- validInput s = case reads s :: [(Int,String)] of
+--   [(n,"")] -> True
+--   _ -> False
